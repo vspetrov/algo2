a6d691e226f4337abc387722bf49b7d01855a437
Valentin Petrov <valentin.petrov@itseez.com>
2013-09-19 22:55:42 +0400

    A2Q2

--- clustering_big_data.py
+++ clustering_big_data.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
-from bitarray import bitarray
-
+import cProfile
 class Vertex:
     """ Vertex of a graph """
     idCounter = 0
@@ -11,7 +10,7 @@ class Vertex:
         Vertex.idCounter += 1
         self.root = self.id
         self.rank = 0
-        self.key = bitarray()
+        self.key = 0
     def findRoot(self):
         if self.root == self.id:
             return self.id
@@ -29,68 +28,87 @@ class Vertex:
             v1.rank += 1
 
     def __str__(self):
-        return "Vertex={id %d, root %d, rank %d}" % (
-            self.id, self.root,self.rank)
+        return "Vertex={id %d, root %d, rank %d, key %d}" % (
+            self.id, self.root,self.rank, self.key)
     def __repr__(self):
         return self.__str__()
     def setKey(self,key):
-        self.key = bitarray(key)
-        Vertex.storage_map[self.key] = self
+        self.key = 0
+        for i in range(24):
+            self.key += int(key[-(i+1)])*(2**i)
+        if self.key in Vertex.storage_map:
+            Vertex.storage_map[self.key].append(self)
+        else:
+            Vertex.storage_map[self.key] = [self]
 
-def buildVariants(key):
-    variants=[]
-    for i in range(24):
-        k = key[:]
-        k[i] = not k[i]
-        variants.append(k)
 
-    for i in range(24):
-        k = key[:]
-        k[i] = not k[i]
-        for j in range(i+1,24):
-            k2 = k[:]
-            k2[j]  = not k2[j]
-            variants.append(k2)
-    return variants
+no_0_dist=False
+no_1_dist=False
+no_2_dist=False
 
 def findClosest():
     v1 = None
     v2 = None
-    distance = 0
-    for v in Vertex.storage:
-        v1 = v
-        key = v.key
-        key_variants = buildVariants(key)
-        d = 0
-        for kv in key_variants[:24]:
-            if kv in Vertex.storage_map:
-                v2 = kv
-                d = 1
-                break
-        if 0 == d:
-            for kv in key_variants[24:]:
-                if kv in Vertex.storage_map:
-                    v2 = kv
-                    d = 2
-                    break
-        if d > 0:
-            distance = d
-            break
-    return v1,v2,distance
+    global no_0_dist
+    global no_1_dist
+    global no_2_dist
+
+    if not no_0_dist:
+        for v in Vertex.storage:
+            v1 = v
+            for vv in Vertex.storage_map[v1.key]:
+                if not vv == v:
+                    v2 = vv
+                    if not v1.findRoot() == v2.findRoot():
+                        return v1,v2
+    if not no_0_dist:
+        no_0_dist = True
+        print "No more 0 dist"
+
+    if not no_1_dist:
+        for v in Vertex.storage:
+            v1 = v
+            for i in range(24):
+                key = v1.key
+                key |= 1<<i
+                if key in Vertex.storage_map:
+                    for vv in Vertex.storage_map[key]:
+                        v2 = vv
+                        if not v1.findRoot() == v2.findRoot():
+                            return v1,v2
+    if not no_1_dist:
+        no_1_dist = True
+        print "No more 1 dist"
+
+    if not no_2_dist:
+       for v in Vertex.storage:
+           v1 = v
+           for i in range(24):
+               key = v1.key | 1<<i
+               for j in range(i,24):
+                   key |= 1<<j
+                   if key in Vertex.storage_map:
+                       for vv in Vertex.storage_map[key]:
+                           v2 = vv
+                           if not v1.findRoot() == v2.findRoot():
+                               return v1,v2
+    return v1,v2
 
 
 def Clusterize():
-    distance = 1
+    go_on=True
     counter=0
-    while (distance > 0):
-        v1,v2,distance = findClosest()
-        if (v1.findRoot() != v2.findRoot() and distance > 0):
+    while (go_on):
+        v1,v2 = findClosest()
+        if ((not None == v1) and (not None == v2)):
             r1 = Vertex.storage[v1.findRoot()]
             r2 = Vertex.storage[v2.findRoot()]
             Vertex.union(r1,r2)
             counter+=1
+        else:
+            go_on = False
         if 100 == counter:
-            print "Vertex.idCounter:",Vertex.idCounter,"dist:",distance
+            print "Vertex.idCounter:",Vertex.idCounter
             counter=0
 
 
@@ -108,13 +126,9 @@ def main():
         Vertex.storage[counter].setKey(key)
         counter+=1
 
-
     print "Number of different keys:", len(Vertex.storage_map)
-#    Clusterize()
-    key = Vertex.storage[0].key
-#    v1,v2,distance = findClosest()
-#    print v1,v2,distance
-    print key in Vertex.storage_map
+    Clusterize()
+
     print "Clusters:",Vertex.idCounter
 
 if __name__ == '__main__':
