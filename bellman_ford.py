9e127ef392e4cb86aca6fb0ea71f32b911db42c4
Valentin Petrov <valentin.petrov@itseez.com>
2013-10-03 09:43:43 +0400

    (Fix) Full solutio to All-pairs shortest-shortest-paths

--- bellman_ford.py
+++ bellman_ford.py
@@ -2,6 +2,7 @@
 import numpypy as np
 import sys
 import copy
+import heapq
 
 class Vertex:
     def __init__(self,id):
@@ -16,7 +17,7 @@ class Edge:
         self.tail = v2
         self.cost = cost
     def __str__(self):
-        return "[%d:%d:%d:%g]"%(self.id,self.head.id,self.tail.id,self.cost)
+        return "e[%d:%d:%d:%g]"%(self.id,self.head.id,self.tail.id,self.cost)
     def __repr__(self):
         return self.__str__()
 
@@ -60,9 +61,9 @@ class Graph:
 
 def bellman_ford(graph,source):
     n = len(graph.vertices)
-    A = np.ones([n,n],'float')*sys.float_info.max
+    A = np.ones([n+1,n],'float')*sys.float_info.max
     A[0,source] = 0
-    for i in range(1,n):
+    for i in range(1,n+1):
         for v in graph.vertices:
             a1 = A[i-1,v.id]
             a2 = sys.float_info.max
@@ -70,7 +71,14 @@ def bellman_ford(graph,source):
                 a3 = A[i-1,e.head.id] + e.cost
                 a2 = a3 if a3 < a2 else a2
             A[i,v.id] = min(a1,a2)
-    return A[n-1]
+
+    has_neg_cycles = False
+    for i in range(n):
+        if not A[n-1,i] == A[n,i]:
+            has_neg_cycles = True
+            break
+
+    return A[n-1] if not has_neg_cycles else []
 
 def reweight(graph,values):
     for e in graph.edges:
@@ -79,30 +87,81 @@ def reweight(graph,values):
         e.cost += values[id1]-values[id2]
         assert(e.cost >= 0)
 
+def get_next(sp,visited,n):
+    minimal = sys.float_info.max
+    ret = -1
+    for i in range(n):
+        if (sp[i] < minimal) and (not visited[i]):
+            ret = i
+            minimal = sp[i]
+    return ret
 
 def dijkstra(graph,source):
     n = len(graph.vertices)
-    sp = np.ones([1,n],'float')*sys.float_info.max
-    sp[source.id]=0
-    visited = np.zeros([1,n],'bool')
-    current = source
+    sp = np.ones([n],'float')*sys.float_info.max
+    sp[source]=0
+    visited = np.zeros([n],'bool')
+    all_visited=False
+    while (not all_visited):
+        next_id = get_next(sp,visited,n)
+        if next_id >= 0:
+            v = graph.vertices[next_id]
+            for e in v.out_edges:
+                w = e.tail
+                sp[w.id] = min(sp[w.id],sp[next_id]+e.cost)
+            visited[next_id]=True
+        else:
+            all_visited = True
+    return sp
 
-def johnson(graph):
-    g = copy.deepcopy(graph)
+
+def johnson(filename):
+    print "Starting Johnson algorithm"
+    graph = Graph(filename)
+    g = Graph(filename)
     v = g.add_vertex()
+    assert(len(g.vertices) == len(graph.vertices)+1)
+
     for i in range(len(graph.vertices)):
         g.add_edge(v,g.vertices[i],0)
+
+    print "Running BellmanFord first"
     short_paths = bellman_ford(g,v.id)
+    if 0 == len(short_paths):
+        print "Current graph has negative cycles"
+        return [],0
     reweight(graph,short_paths[:-1])
+    sp = []
+    shortest_shortest_path = sys.float_info.max
+    step = int(len(graph.vertices)/100.0)+1
+    for i in range(len(graph.vertices)):
+        shortest_path = dijkstra(graph,i)
+        for j in range(len(shortest_path)):
+            shortest_path[j] += short_paths[j]-short_paths[i]
+            if shortest_path[j] < shortest_shortest_path and not i == j:
+                shortest_shortest_path = shortest_path[j]
+            if shortest_path[j] > 1e300:
+                shortest_path[j] = None
+            else:
+                shortest_path[j] = int(shortest_path[j])
+        sp.append(shortest_path)
+        if i/step*step == i:
+            print "{0:d} % done..".format(int(float(i)/(len(graph.vertices)/100.0)))
 
-    for v in graph.vertices:
-        v.out_edges = sorted(v.out_edges,key=lambda e: e.cost)
-
-    return 0
+    return sp,shortest_shortest_path
 
 def main():
-    g = Graph('data/clustering_test.txt')
-    answ = johnson(g)
+    sp,ssp = johnson('data/g1.txt')
+    # for s in sp:
+        # print ["{0:0.2f}".format(i) for i in s]
+
+    print "SSP: ", ssp
+    sp,ssp = johnson('data/g2.txt')
+    print "SSP: ", ssp
+
+    sp,ssp = johnson('data/g3.txt')
+    print "SSP: ", ssp
+
 
 if __name__ == '__main__':
     main()
